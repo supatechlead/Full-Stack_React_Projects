3349994c86325d036fbf306b1002be9144d5bc51
s-techlead <sebastienschnd.techlead@gmail.com>
2023-04-08 18:50:17 +0200

    First commit

---
+++ 02-mern-skelton/server/express.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+import Template from './../template'
+import userRoutes from './routes/user.routes'
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
+// mount routes
+app.use('/', userRoutes)
+app.use('/', authRoutes)
+
+app.get('/', (req, res) => {
+    res.status(200).send(Template)
+})
+
+export default app
